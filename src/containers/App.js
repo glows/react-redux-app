2a47523eb215ecbbf1b8deef7a98ac437d9ea2ce
duncyun <lawmusil@163.com>
2018-05-16 18:14:09 +0800

    init

---
+++ src/containers/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { selectSubreddit, fetchPostsIfNeeded, invalidateSubreddit } from "../actions";
+import Picker from "../components/Picker";
+
+class App extends Component {
+  static propTypes = {
+    selectedSubreddit: PropTypes.string.isRequired,
+    posts: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    lastUpdated: PropTypes.number,
+    dispatch: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selectedSubreddit !== this.props.selectedSubreddit) {
+      const { dispatch, selectedSubreddit } = nextProps;
+      dispatch(fetchPostsIfNeeded(selectedSubreddit));
+    }
+  }
+
+  handleChange = nextSubreddit => {
+    this.props.dispatch(selectSubreddit(nextSubreddit))
+  }
+
+  handleRefreshClick = e => {
+    e.preventDefault()
+
+    const { dispatch, selectedSubreddit } = this.props
+    dispatch(invalidateSubreddit(selectedSubreddit))
+    dispatch(fetchPostsIfNeeded(selectedSubreddit))
+  };
+  render() {
+    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props;
+    // const isEmpty = posts.length === 0
+    return (
+      <div>
+        <Picker
+          value={selectedSubreddit}
+          onChange={this.handleChange}
+          options={["reactjs", "frontend", "javascript", "vuejs"]}
+        />
+        <p>
+          {lastUpdated && 
+            <span>
+              Last updated at {new Date(lastUpdated).toLocaleDateString()}.
+              {" "}
+            </span>
+          }
+          {!isFetching && (
+            <button onClick={this.handleRefreshClick}>Refresh </button>
+          )}
+        </p>
+
+      </div>
+    );
+  }
+}
+
+const mapStateProps = state => {
+  const { selectedSubreddit, postsBySubreddit } = state;
+
+  const  {
+      isFetching,
+      lastUpdated,
+      item: posts
+  } = postsBySubreddit[selectedSubreddit]  || {
+      isFetching: true,
+      items: []
+  }
+  return {
+    selectedSubreddit,
+    posts,
+    isFetching,
+    lastUpdated
+  };
+};
+
+export default connect(mapStateProps)(App);
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createLogger } from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+
+const middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
